3857bf93e5b8dd129797f572445c34537a93415c
Lakshay Sachdeva <lakshay07sachdeva@gmail.com>
2021-11-08 23:32:39 +0530

    navbar

---
+++ components/navbar.js
@@ -0,0 +1,15 @@
+function navbar () {
+    return `
+    <div>burger menu</div>
+        <div>
+            <h1>ZARA</h1>
+        </div>
+        <div>Search</div>
+        <div class="right">
+            <div>LOG IN</div>
+            <div>HELP</div>
+            <div>CART</div>
+        </div>`
+}
+
+export default navbar ;
