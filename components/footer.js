b10dba1083cafaa456fb1d476f41d075b121fe7a
Lakshay Sachdeva <lakshay07sachdeva@gmail.com>
2021-11-09 23:41:39 +0530

    footer

--- components/footer.js
+++ components/footer.js
@@ -1,6 +1,21 @@
 function footer(){
     return `
     <div>
-        
-    </div>`
+        <label>JOIN OUR NEWSLETTER</label>
+        <br/>
+        <input id="news_letter" type="text" placeholder="ENTER YOUR EMAIL ADDRESS HERE" />
+    </div>
+    <div>
+        <p>TIKTOK</p>
+        <p>INSTAGRAM</p>
+        <p>FACEBOOK</p>
+        <p>TWITTER</p>
+        <p>PINTEREST</p>
+        <p>YOUTUBE</p>
+    </div>
+    <hr/>
+    <h6>NAME AND ADDRESS OF THE MANUFACTURER:</h6>
+    <p>INDUSTRIA DE DISENO TEXTIL, S.A. (INDITEX, S.A.)<br/>AVENIDA DE LA DIPUTACTION, EDIFICIO INDITEX, 15143, ARTEIXO (A CORUNA), SPAIN</p>`;
 }
+
+export default footer ;
