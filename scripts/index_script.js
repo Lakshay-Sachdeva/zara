c0db8c61c9f955ca00b6fc93f15cdb92968d8562
Lakshay Sachdeva <lakshay07sachdeva@gmail.com>
2021-11-12 03:31:58 +0530

    index page script

---
+++ scripts/index_script.js
@@ -0,0 +1,40 @@
+
+let categ = [
+    {
+        image:"./images/man.webp",
+        name:"Man",
+        description:"Introducing the Fall/Winter 2021 Collection"
+    },
+    {
+        image:"./images/woman.jpg",
+        name:"Woman",
+        description:"Introducing the Fall/Winter 2021 Collection"
+    },
+    {
+        image:"./images/kids.jpg",
+        name:"Kids",
+        description:"Introducing the Fall/Winter 2021 Collection"
+    }
+]
+
+let parent_div = document.getElementById("categories") ;
+
+categ.forEach(el=>{
+
+    let category_div = document.createElement("div") ;
+    category_div.onclick = () =>{
+        window.location = "./products.html" ;
+    }
+
+    let image = document.createElement("img") ;
+    image.src = el.image
+
+    let caption = document.createElement("p") ;
+    caption.innerText = el.description
+
+
+    category_div.append(image , caption) ;
+
+    parent_div.append(category_div) ;
+}) ;
+
