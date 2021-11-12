cb8b0978364f830c791036eae27895299b81b6e8
Lakshay Sachdeva <lakshay07sachdeva@gmail.com>
2021-11-12 18:02:03 +0530

    minor tweaks

--- components/navbar.js
+++ components/navbar.js
@@ -2,7 +2,7 @@ function navbar () {
     return `
     <div><img src="https://img.icons8.com/material-outlined/24/000000/menu--v1.png"/></div>
             <div id="logo">
-                <img src="./images/zara.png" />
+                <a href="index.html"><img src="./images/zara.png" /></a>
             </div>
             <a href="./search.html"><div>Search</div></a>
             <div class="right">
